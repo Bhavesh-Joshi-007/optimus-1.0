75b2aec3f1b8be1b11744f2b620dd74d2206ecc0
Bhavesh Joshi <upscalewithbhavesh@gmail.com>
2026-05-26 19:26:39 +0530

    Initialised the Basic UI Functionality

--- UI/assets/main.js
+++ UI/assets/main.js
@@ -1,59 +1,158 @@
-// Floating animation
-gsap.to(".jarvis", {
-  y: -15,
-  duration: 2,
-  repeat: -1,
-  yoyo: true,
-  ease: "power1.inOut",
-});
+window.addEventListener("load", () => {
+  // Initialize Lucide Icons
+  lucide.createIcons();
 
-// Core pulse
-gsap.to(".core", {
-  scale: 1.08,
-  duration: 1.8,
-  repeat: -1,
-  yoyo: true,
-  ease: "sine.inOut",
-});
+  // Floating animation
+  gsap.to(".OPTIMUS", {
+    y: -15,
+    duration: 2,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut",
+  });
 
-// Dot Orbit
-// gsap.to(".dot", {
-//   rotation: 360,
-//   transformOrigin: "250px 0px",
-//   repeat: -1,
-//   duration: 5,
-//   ease: "linear",
-// });
+  // Core pulse
+  gsap.to(".core", {
+    scale: 1.08,
+    duration: 1.8,
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut",
+  });
 
-// Status panel animation
-gsap.from(".status", {
-  x: 100,
-  opacity: 0,
-  duration: 1.5,
-  ease: "power4.out",
-});
+  // Dot Orbit
+  // gsap.to(".dot", {
+  //   rotation: 360,
+  //   transformOrigin: "250px 0px",
+  //   repeat: -1,
+  //   duration: 5,
+  //   ease: "linear",
+  // });
 
-// Title Animation
-gsap.from(".title", {
-  opacity: 0,
-  y: 30,
-  duration: 1.5,
-  ease: "power4.out",
-});
+  // Status panel animation
+  // gsap.from(".status", {
+  //   x: 100,
+  //   opacity: 0,
+  //   duration: 1.5,
+  //   ease: "power4.out",
+  // });
 
-gsap.from(".subtitle", {
-  opacity: 0,
-  y: 20,
-  duration: 2,
-  delay: 0.3,
-  ease: "power4.out",
-});
+  // Title Animation
+  gsap.from(".title", {
+    opacity: 0,
+    y: 30,
+    duration: 1.5,
+    ease: "power4.out",
+  });
 
-// HUD line flicker
-gsap.to(".hud-line", {
-  opacity: 0.2,
-  repeat: -1,
-  yoyo: true,
-  stagger: 0.2,
-  duration: 0.6,
+  // gsap.from(".subtitle", {
+  //   opacity: 0,
+  //   y: 20,
+  //   duration: 2,
+  //   delay: 0.3,
+  //   ease: "power4.out",
+  // });
+
+  // HUD line flicker
+  gsap.to(".hud-line", {
+    opacity: 0.2,
+    repeat: -1,
+    yoyo: true,
+    stagger: 0.2,
+    duration: 0.6,
+  });
+
+  /* =========================
+   SIRI WAVE
+========================= */
+
+  const siriWaveSection = document.getElementById("SiriWave");
+
+  const optimusSection = document.querySelector(".OPTIMUS");
+
+  const micBtn = document.querySelector(".mic-btn");
+
+  // CREATE WAVE
+
+  const siriWave = new SiriWave({
+    container: document.querySelector(".siri-container"),
+    width: 700,
+    height: 250,
+    style: "ios9",
+    speed: 0.12,
+    amplitude: 1,
+    autostart: true,
+    cover: false,
+  });
+
+  // siriWave.start();
+
+  // MIC CLICK EVENT
+  /* =========================
+   MIC BUTTON CLICK
+========================= */
+
+  let listening = false;
+
+  micBtn.addEventListener("click", () => {
+    if (!listening) {
+      listening = true;
+
+      // =========================
+      // HIDE OPTIMUS
+      // =========================
+
+      gsap.to(".OPTIMUS", {
+        opacity: 0,
+        scale: 0.7,
+        duration: 0.8,
+        ease: "power4.inOut",
+        onComplete: () => {
+          optimusSection.style.visibility = "hidden";
+        },
+      });
+
+      // =========================
+      // SHOW SIRI WAVE
+      // =========================
+
+      siriWaveSection.style.visibility = "visible";
+
+      gsap.to("#SiriWave", {
+        opacity: 1,
+        duration: 1,
+        delay: 1,
+        ease: "power4.out",
+      });
+    } else {
+      listening = false;
+
+      // =========================
+      // HIDE SIRI WAVE
+      // =========================
+
+      gsap.to("#SiriWave", {
+        opacity: 0,
+        duration: 0.8,
+        ease: "power4.inOut",
+        onComplete: () => {
+          siriWaveSection.style.visibility = "hidden";
+        },
+      });
+
+      // =========================
+      // SHOW OPTIMUS
+      // =========================
+
+      optimusSection.style.visibility = "visible";
+
+      gsap.to(".OPTIMUS", {
+        opacity: 1,
+        scale: 1,
+        duration: 1,
+        delay: .7,
+        ease: "power4.out",
+      });
+    }
+  });
 });
