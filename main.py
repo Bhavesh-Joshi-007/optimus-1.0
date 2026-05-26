584084037ab2a8289b55a40e83b8459ec5c23e42
Bhavesh Joshi <upscalewithbhavesh@gmail.com>
2026-05-26 13:42:32 +0530

    setting up complete with the virtual environment

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import os
+
+import eel
+
+eel.init("UI")
+
+# os.system('open -a "Google Chrome" "http://localhost:8000/index.html"')
+
+eel.start('index.html', mode='chrome', host='localhost', block=True)
+
  
